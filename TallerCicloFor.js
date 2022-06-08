bfadaad23524bf473e74aefa9be52dabb714c4aa
Santiago2635 <santiagomenruiz@gmail.com>
2022-06-08 10:24:27 -0500

    Taller ciclos

---
+++ TallerCicloFor.js
@@ -0,0 +1,52 @@
+console.log("punto 1")
+//Diseñe un algoritmo que reciba 2 números sin ningún orden. El algoritmo debe contener un ciclo for que imprima los números consecutivos ascendentemente, cuyo rango está dado por los dos números que ingresó el usuario. Ejemplo: el usuario ingresa el 38 y el 5, el ciclo for debe imprimir 5, 6, 7, 8, …, 38
+
+var nume1=parseInt(prompt("Ingrese un numero"))
+var nume2=parseInt(prompt("Ingrese otro numero"))
+
+if(nume1<nume2){
+    for(var i=nume1; i<=nume2; i+=1){
+    console.log(i);
+ 
+}
+}else(nume2<nume1)
+for(var i=nume2; i<=nume1; i+=1){
+    console.log(i);
+ }
+
+ console.log("punto 2")
+
+/* Escriba un algoritmo que reciba un número e imprima la tabla de multiplicar desde el cero hasta el 10 de ese número. Ejemplo: ingresa el 5, imprime 
+5x0=0
+5x1=5
+…
+5x10=50
+*/
+
+var nume0=parseInt(prompt("Ingrese un numero del 0 al 10"))
+for(var i=0;i<=10;i++){
+    console.log(nume0+"x"+i+"="+nume0*i)
+}
+
+console.log("punto 3")
+var numer1=parseInt=(prompt("ingrese el numero que desea multiplicar"))
+var numer2=parseInt=(prompt("ingrese el numero hasta el que quiere miltiplicar"))
+for(var i=0;i<=numer2;i++){
+    console.log(numer1+"x"+i+"="+numer1*i)
+}
+
+
+console.log("Punto 4")
+//Diseñe un ciclo for que reciba 10 números y los guarde en un arreglo, escriba otro ciclo for que retorne el número mayor del arreglo.
+
+var mayor=arregloNumeros=[] 
+var numeros; 
+for(var i=1; i<=10; i++){ 
+    numeros=prompt("Ingrese un número "+i)
+    arregloNumeros.push(numeros); 
+    if(arregloNumeros[i]>mayor){
+        mayor=arregloNumeros[i]
+        console.log("El numero mayor registrado es "+mayor)
+    }
+}
+
